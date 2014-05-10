2086dd6001a97780c86409cbfbf4c2adfdd4d457
Albert Lim <albertlim322@gmail.com>
2014-05-09 22:15:28 -0700

    wip

--- src/main/java/server/EventHandler.java
+++ src/main/java/server/EventHandler.java
@@ -11,18 +11,20 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
  */
 public class EventHandler extends ChannelInboundHandlerAdapter { // (1)
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
-        // Discard the received data silently.
-        ((ByteBuf) msg).release(); // (3)
-        ctx.write(msg);
-        ctx.flush();
-    }
+	 @Override
+	    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+	        // Discard the received data silently.
+		 ByteBuf in = (ByteBuf) msg;
+	        while (in.isReadable()) { // (1)
+	            System.out.print((char) in.readByte());
+	            System.out.flush();
+	        }
+	    }
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
-        // Close the connection when an exception is raised.
-        cause.printStackTrace();
-        ctx.close();
-    }
+	    @Override
+	    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+	        // Close the connection when an exception is raised.
+	        cause.printStackTrace();
+	        ctx.close();
+	    }
 }
